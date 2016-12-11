ace786cf58ea2877aaa84c10a015298d597561ef
Alexey Zorchenkov <alexey@t5p.root>
2016-12-11 13:29:55 +0800

    Week2 home work done

--- week2/my-project/app/Main.hs
+++ week2/my-project/app/Main.hs
@@ -3,18 +3,14 @@ module Main where
 import LogAnalysis
 import Log
 
+
+--Exercise 1
 x = testParse parse 10 "E:\\DEV\\Haskell\\CIS194\\week2\\my-project\\resources\\error.log"
 -- parseMessage "E 2 562 help help" == LogMessage (Error 2) 562 "help help"
 -- parseMessage "E 2 3151 fond of beheading people here; the great wonder is, that there's any one"
 
--- show
-baz :: LogMessage -> String
-baz p@(LogMessage _ _ c) = "The content field of (" ++ show p ++ ") is " ++ c
 
-
-convertmsgtostr :: LogMessage -> IO()
-convertmsgtostr msg = putStrLn (baz msg)
-
+--Exercise 5: Just run main in REPL to see results
 main :: IO ()
 main = do
   list <- parse <$> readFile "E:\\DEV\\Haskell\\CIS194\\week2\\my-project\\resources\\sample.log"
