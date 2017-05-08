50be3c985d0eee3d2607b463930beaa719fe77f6
unknown <>
2017-05-08 12:50:38 +0300

    Upgrading to Haskell 8.0.2

--- week1/my-project/src/Creditcard.hs
+++ week1/my-project/src/Creditcard.hs
@@ -10,22 +10,22 @@ module Creditcard
 
 double_function :: (Integer,Int) -> Integer
 double_function (x,y) = if y `mod` 2 == 1
-					then (2 * x )
-					else x
+                                        then (2 * x )
+                                        else x
 
 
 sumdigits_function :: Integer -> Integer
 sumdigits_function x = if x > 10
-					then x `mod` 10 + sumdigits_function (quot x 10)
-					else x
+                                        then x `mod` 10 + sumdigits_function (quot x 10)
+                                        else x
 
 array_to_index_tuple :: [Integer] -> [Integer]
 array_to_index_tuple a = map double_function (zip a [0..])
 
 toDigits :: Integer -> [Integer]
 toDigits x
-	| x <= 0 		= []
-	| otherwise     = toDigits (x `div` 10) ++ [x `mod` 10]
+        | x <= 0        = []
+        | otherwise     = toDigits (x `div` 10) ++ [x `mod` 10]
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther a = array_to_index_tuple a
@@ -38,5 +38,5 @@ sumDigits a = sum(map sumdigits_function a)
 
 validate :: Integer -> Bool
 validate x = if x `mod` 10 == 0
-		then True
-		else False
+                then True
+                else False
